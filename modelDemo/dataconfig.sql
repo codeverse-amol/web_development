92e2fbe58aababcec7b3eac5de9b585c238016f6
codeverse-amol <amolbs2109@gmail.com>
2026-05-17 22:12:27 +0530

    operated ORM relationships using shell

---
+++ modelDemo/dataconfig.sql
@@ -0,0 +1,24 @@
+create database employeedb
+show databases
+use employeedb
+show tables
+
+select * from empApp_employee
+
+DELETE FROM empApp_employee
+WHERE id = 4;
+
+insert into empApp_employee values(1, 'Amol', 'Bandgar', 450000, 'amol@gmail.com')
+
+
+select * from empapp_programmer
+
+select * from empapp_project
+
+select * from empapp_project_programmers
+
+
+select * from empapp_phonenumber
+
+select * from empapp_person
+select * from empapp_license
--- modelDemo/empApp/models.py
+++ modelDemo/empApp/models.py
@@ -11,3 +11,42 @@ class Employee(models.Model):
 
 # e = Employee(firstName='John', lastName='Doe', salary=50000.0, email='john.doe@example.com')
 # e.save()  # This will save the Employee instance to the database. You can run this code in a Django shell to create a new Employee record.
+
+
+
+class Programmer(models.Model):
+    name = models.CharField(max_length=20)
+    sal = models.IntegerField()
+
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=20)
+    programmers = models.ManyToManyField(Programmer)
+
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class PhoneNumber(models.Model):
+    type = models.CharField(max_length=10)
+    number = models.CharField(max_length=15)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+
+class Person(models.Model):
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    age = models.IntegerField(max_length=20)
+
+
+
+class License(models.Model):
+    type = models.CharField(max_length=20)
+    license_number = models.CharField(max_length=30)
+    validFrom = models.DateField()
+    validTo = models.DateField()
+    person = models.OneToOneField(Person, on_delete=models.CASCADE)
