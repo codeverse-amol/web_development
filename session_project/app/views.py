cb3f74be882c7325a8ed26f6909a4d015ad2938f
codeverse-amol <amolbs2109@gmail.com>
2026-05-18 22:46:17 +0530

    updated project

--- session_project/app/views.py
+++ session_project/app/views.py
@@ -11,6 +11,7 @@ from django.contrib.auth import authenticate, login, logout
 
 
 # LOGIN VIEW
+# The login_view function handles user authentication. It checks if the request method is POST, retrieves the username and password from the request, and uses Django's authenticate function to verify the credentials. If authentication is successful, it logs the user in and redirects them to the dashboard. If authentication fails, it returns an error message. If the request method is not POST, it renders the login page.
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -52,27 +53,26 @@ def login_view(request):
 
 @login_required          # This decorator ensures that only authenticated users can access the dashboard view. If a user is not authenticated, they will be redirected to the login page.
 def dashboard_view(request):
-
     return render(request, 'app/index.html')
 
-
-
-
+# get_active_user function checks if the user is authenticated and returns the user object. If the user is not authenticated, it returns the last created user from the database. This function can be used to retrieve the active user in various views.
 def get_active_user(request):
     if request.user.is_authenticated:
         return request.user
     return User.objects.last()
 
-
+# get_or_create_cart function retrieves the cart associated with the user. If the cart does not exist, it creates a new cart for the user. This function is useful for managing the user's shopping cart in the application.
 def get_or_create_cart(user):
     cart, _ = Cart.objects.get_or_create(user=user)
     return cart
 
 
+@login_required
 def index(request):
     return render(request, "app/index.html")
 
 
+@login_required
 def create_user(request):
     if request.method=="POST":
         form = UserForm(request.POST)
@@ -85,13 +85,13 @@ def create_user(request):
         form = UserForm()
     return render(request, "app/create_user.html", {'form':form})
 
-
+# This view function, create_user, handles the creation of new user accounts. It checks if the request method is POST, and if so, it processes the submitted form data. If the form is valid, it creates a new user object, sets the password using Django's set_password method (which hashes the password), and saves the user to the database. After successful creation, it redirects to the 'new_user' view. If the request method is not POST, it renders an empty user creation form.
 def new_user(request):
     users = User.objects.all()
     return render(request, "registration/new_user.html", {'users':users})
 
 
-# @login_required
+@login_required
 def create_profile(request):
     if request.method=="POST":
         form = ProfileForm(request.POST)
@@ -110,7 +110,7 @@ def create_profile(request):
 
 
 
-
+@login_required
 def add_category(request):
     if request.method=="POST":
         form = CategoryForm(request.POST)
@@ -179,9 +179,7 @@ def add_to_cart(request, product_id):
 
 @login_required
 def view_cart(request):
-
     cart = get_or_create_cart(request.user)
-
     cart_items = cart.items.all()
 
     return render(request, 'app/cart.html', {
@@ -224,7 +222,7 @@ def order_success(request):
 
 
 # LOGOUT VIEW
-@login_required
+@login_required 
 def logout_view(request):
 
     # DESTROY SESSION
